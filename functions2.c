3bf77990ad1d8e0327e9956c4b9842fac0e8b30d
3nzio <Eezy911@gmail.com>
2023-07-24 22:11:21 +0200

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -9,6 +9,7 @@
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
+ * by Mohamed and Enzio
  * Return: Number of chars printed.
  */
 int print_pointer(va_list types, char buffer[],
