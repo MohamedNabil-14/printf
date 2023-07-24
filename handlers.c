8cbbfe0c25c24501e6109f96534443fb95763f7e
3nzio <Eezy911@gmail.com>
2023-07-24 22:13:33 +0200

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -8,6 +8,7 @@
  * @flags:  Calculates active flags.
  * @width: get width.
  * @precision: precision specifier
+ * by Mohamed and Enzio
  * @size: Size specifier
  *
  * Return: Number of chars printed.
